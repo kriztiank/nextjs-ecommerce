aa17f5b56996783d6bfd6326ea8f506547a411fa
kriztiank <kriztiank@gmail.com>
2023-08-03 11:08:48 +0200

    Products list page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <main className="min-w[300px] m-auto max-w-7xl p-4">{children}</main>
+        <main className="m-auto min-w-[300px] max-w-7xl p-4">{children}</main>
       </body>
     </html>
   );
